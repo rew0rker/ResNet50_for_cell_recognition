73832874f80cc04ee109cdaefee442bb691575f8
hulio <huchhuch.com@gmail.com>
2023-01-19 21:55:04 +0500

    add saving metrics and model in file, add graphs of acc and loss

--- resnet.py
+++ resnet.py
@@ -9,8 +9,10 @@ from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 import numpy as np
+import csv
 
-batch_size = 256
+
+batch_size = 8
 image_size = (224, 224)
 
 train_ds = image_dataset_from_directory('data',
@@ -28,7 +30,7 @@ validation_ds = image_dataset_from_directory('data',
                                              image_size=image_size)
 
 test_ds = image_dataset_from_directory('data',
-                                       seed=0,
+                                       seed=42,
                                        subset='both',
                                        validation_split=0.1,
                                        batch_size=batch_size,
@@ -69,13 +71,14 @@ base_model.trainable = False
 
 # Создаем новый верхний выходной слой
 inputs = keras.Input(shape=(224, 224, 3))
-x = data_augmentation(inputs)  # добавляем рандомную аугментацию данных
+x = data_augmentation(inputs) # добавляем рандомную аугментацию данных
+
 
 # Базовая модель содержит слои пакетной нормы. Мы хотим, чтобы они оставались в режиме вывода
 # когда мы разморозим базовую модель для тонкой настройки, поэтому мы убедимся, что base_model здесь работает в режиме вывода.
 x = base_model(x, training=False)
 # x = keras.layers.GlobalAveragePooling2D()(x)
-x = keras.layers.Dropout(0.2)(x)  # Регуляризация с отсевом
+x = keras.layers.Dropout(0.1)(x)  # Регуляризация с отсевом
 outputs = keras.layers.Dense(1)(x)  # создаем выходной слой(dense -просто распростарненный слой)
 model = keras.Model(inputs, outputs)
 
@@ -87,7 +90,7 @@ model.compile(
     loss=keras.losses.BinaryCrossentropy(from_logits=True),
     metrics=[keras.metrics.BinaryAccuracy()],
 )
-epochs = 10  # test various variations
+epochs = 20  # test various variations
 model.fit(train_ds, epochs=epochs, validation_data=validation_ds)
 
 # Разморозить base_model. Обратите внимание, что он продолжает работать в режиме вывода
@@ -99,10 +102,40 @@ base_model.trainable = True
 model.summary()
 
 model.compile(
-    optimizer=keras.optimizers.Adam(1e-5),  # Low learning rate
+    optimizer=keras.optimizers.Adam(2e-6),  # Low learning rate
     loss=keras.losses.BinaryCrossentropy(from_logits=True),
     metrics=[keras.metrics.BinaryAccuracy()],
 )
 
-epochs = 10
+epochs = 100
 model.fit(train_ds, epochs=epochs, validation_data=validation_ds)
+
+model.save("my_model1")
+
+
+# Запись метрик в файл
+ls_acc = model.fit.history['accuracy']
+ls_los = model.fit.history['loss']
+with open('config1', 'w') as fileptr:
+    writer = csv.writer(fileptr, delimiter=",", lineterminator="\r")
+    writer.writerow(['epoch', 'loss', 'accuracy'])
+    for i in range(99):
+        writer.writerow([i+1, ls_los[i], ls_acc[i]])
+
+
+plt.plot(ls_los, label="Конфигурация 1", color="red", marker=".")
+plt.xlabel("Эпоха")
+plt.ylabel("Потери")
+plt.legend()
+plt.savefig("loss1.png")
+plt.cla()
+plt.clf()
+
+plt.plot(ls_los, label="Конфигурация 1", color="green", marker=".")
+plt.xlabel("Эпоха")
+plt.ylabel("Точность")
+plt.legend()
+plt.savefig("acc1.png")
+plt.cla()
+plt.clf()
+
